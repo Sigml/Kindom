ca91d027e65d40fc9135fc74e22e3900d25a4d24
Sigml <sigmll1994@gmail.com>
2024-03-17 15:37:28 +0100

    create register view

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterUserView, LoginUserView, LogoutUserView
 # from views import RegisterUserView, LoginUserView, LogoutUserView
 
 urlpatterns = [
-    path('user/register/', RegisterUserView.as_view(), name = 'register'),
+    path('register/', RegisterUserView.as_view(), name = 'register'),
     path('login/', LoginUserView.as_view(), name = 'login'),
     path('user/logout/', LogoutUserView.as_view(), name = 'logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CustomUser
 from django.contrib.auth.tokens import default_token_generator as token_generator, default_token_generator
 from django.utils.http import urlsafe_base64_decode
-# from .utils import send_email_verify, send_email_reset_password
+from .utils import send_email_verify, send_email_reset_password
 from .forms import RegisterUserForm
 from django.contrib.auth import login, logout
 from django.views import View
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.http import HttpResponseServerError
 
 
 class RegisterUserView(View):
@@ -15,10 +18,31 @@ class RegisterUserView(View):
             'form':form
         }
         return render(request, 'registration.html', context)
+    
+    def post(self, request):
+        try:
+            form = RegisterUserForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                password = data.pop('password_confirmation')
+                
+                user = CustomUser.objects.create_user(**data)
+                user.set_password
+                send_email_verify(request.user)
+                return redirect
+            else:
+                context = {'form': form}
+                return render(request, 'registration.html', context)
+        except IntegrityError as e:
+                return HttpResponseServerError('Użytkownik już istnieje')
+        except Exception as e:
+                message = "Coś poszło nie tak, spróbuj ponownie później"
+                return render(request, 'registration.html', {'message': message})
 
 
 class LoginUserView(View):
-    pass
+    def get(self, request):
+        return render(request, 'login.html')
 
 
 class LogoutUserView(View):
