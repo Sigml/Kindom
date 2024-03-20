4b60efda5697447b0423c31814ec0b08e138fe1b
Sigml <sigmll1994@gmail.com>
2024-03-20 10:20:48 +0100

    User Registration

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from .views import RegisterUserView, LoginUserView, LogoutUserView, EmailVerifyView, UserInfoView
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name = 'register'),
+    path('registration/', RegisterUserView.as_view(), name = 'registration'),
     path('confirm_email/', TemplateView.as_view(template_name='confirm_email.html'), name='confirm_email'),
     path('verify_email/<uidb64>/<token>/', EmailVerifyView.as_view(), name='verify_email'),
     path('invalid_verify/', TemplateView.as_view(template_name='invalid_verify.html'), name='invalid_verify'),
